73a6656f2fdb24bde7431ad8d74e0b1f0ca539e0
Matheus Paiva <mmccpp@gmail.com>
2017-10-24 23:59:28 -0200

    removed lodash dependency and es6 (for retro compatibility)

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const _ = require('lodash')
-
-const _options = {
+const _config = {
   errorMessage: ({ path }) => `Access denied to ${path}.`
 }
 
-module.exports = function (schema, options = _options) {
+module.exports = function (schema, config = _config) {
   schema.methods.actor = function (actor) {
     this._actor = actor
     return this
@@ -15,30 +13,28 @@ module.exports = function (schema, options = _options) {
     return this
   }
 
-  const _pathsToValidate = _.chain(schema.paths)
-    .values()
-    .map(path => path.options && path.options.validateAccess ? {
-      path: path.path,
-      validateAccess: path.options.validateAccess
-    } : void (0))
-    .filter(item => item)
-    .value()
+  const _pathsToValidate = Object
+    .keys(schema.paths)
+    .map(val => schema.paths[val])
+    .filter(item => item.options && item.options.validateAccess)
+    .map(({ path, options }) => ({ path, validateAccess: options.validateAccess }))
 
-  const _methodsToValidate = Object.keys(
-    _pathsToValidate
-      .map(item => item.validateAccess)
-      .reduce((a, b) => ({ ...a, ...b }), {})
-  )
+  const _methodsToValidate = Object
+    .keys(
+      _pathsToValidate
+        .map(item => item.validateAccess)
+        .reduce((a, b) => Object.assign({}, a, b), {})
+    )
 
   _methodsToValidate.forEach(method => {
     schema.pre(method, function (next) {
       const self = this._doc || this._update
-      if (!_.isNil(this._actor)) _pathsToValidate
-        .map(path => ({ ...path, validateAccess: path.validateAccess[method] }))
-        .filter(item => _.has(self, item.path) && item.validateAccess)
-        .map(item => ({ ...item, value: self[item.path] }))
-        .map(item => ({ ...item, isValid: item.validateAccess(item.value, this._actor) }))
-        .forEach(item => item.isValid ? void (0) : next(new Error(options.errorMessage(item))))
+      if (this._actor) _pathsToValidate
+        .map(item => Object.assign({}, item, { validateAccess: item.validateAccess[method] }))
+        .filter(item => self.hasOwnProperty(item.path) && item.validateAccess)
+        .map(item => Object.assign({}, item, { value: self[item.path] }))
+        .map(item => Object.assign({}, item, { isValid: item.validateAccess(item.value, this._actor) }))
+        .forEach(item => item.isValid ? void (0) : next(new Error(config.errorMessage(item))))
       next()
     })
   })
